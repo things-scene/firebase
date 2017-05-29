d863047c2f4add783a2ca133cfe8c84a302fc043
heartyoh <heartyoh@gmail.com>
2017-05-29 23:39:02 +0900

    initial import

---
+++ src/firebase.js
@@ -0,0 +1,80 @@
+const NATURE = {
+  mutable: false,
+  resizable: true,
+  rotatable: true,
+  properties : [{
+    type: 'number',
+    label: 'value',
+    name: 'value',
+    property: 'value'
+  },{
+    type: 'angle',
+    label: 'angle property',
+    name: 'propAngle',
+    property: 'propAngle'
+  },{
+    type: 'string',
+    label: 'string property',
+    name: 'propString',
+    property: 'propString'
+  },{
+    type: 'color',
+    label: 'color property',
+    name: 'propColor',
+    property: 'propColor'
+  }]
+}
+
+var { ValueHolder, RectPath, Shape } = scene
+
+export default class Firebase extends ValueHolder(RectPath(Shape)) {
+
+  _draw(context) {
+    var {
+      top,
+      left,
+      height,
+      width,
+      backgroundColor = 'transparent',
+      reverse
+    } = this.model;
+
+    this.animOnValueChange(this.value);
+
+    // background의 색상
+    context.beginPath();
+    context.rect(left, top, width, height);
+
+    context.fillStyle = backgroundColor;
+    context.fill();
+
+    // value의 색상
+    context.beginPath();
+
+    var drawValue = width - width * Math.max(Math.min(this.animValue, 100), 0) / 100;
+    drawValue = Math.max(Math.min(drawValue, width), 0);
+
+    context.rect(left + drawValue, top, width - drawValue, height);
+
+    this.drawFill(context);
+
+    context.closePath();
+
+    context.beginPath();
+
+    context.rect(left, top, width, height);
+  }
+
+  _post_draw(context) {
+    this.drawStroke(context);
+    this.drawText(context);
+  }
+
+  get controls() {}
+
+  get nature(){
+    return NATURE;
+  }
+}
+
+scene.Component.register('firebase', Firebase);
---
+++ test/unit/test-firebase.js
@@ -0,0 +1,29 @@
+import './util'
+
+import { expect } from 'chai'
+
+import '../../bower_components/things-scene-core/things-scene-min'
+import { Firebase } from '../../src/index'
+
+describe('Firebase', function () {
+
+  var board;
+
+  beforeEach(function () {
+    board = scene.create({
+      model: {
+        components: [{
+          id: 'firebase',
+          type: 'firebase'
+        }]
+      }
+    })
+  });
+
+  it('component should be found by its id.', function () {
+
+    var component = board.findById('firebase')
+
+    expect(!!component).not.to.equal(false);
+  });
+});
